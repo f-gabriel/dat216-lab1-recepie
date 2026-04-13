40bb1940d7919d29cbc0ebc84644724dd08e9cb9
Fredrik <fredn@chalmers.se>
2026-04-13 10:46:41 +0200

    kod klar

--- lab2/lib/pages/recipe_detail.dart
+++ lab2/lib/pages/recipe_detail.dart
@@ -12,11 +12,22 @@ class RecipeDetail extends StatelessWidget{
   Widget build(BuildContext context) {
     var uiController = Provider.of<UiController>(context, listen: false);
     
-    return IconButton(
-      onPressed: (){
-        uiController.deselectRecipe();
-      }, 
-      icon: Icon(Icons.close)
+    return Row(children: [
+      SizedBox(
+        height: 200,
+        width: 300,
+        child: recipe.image
+          ),
+      SizedBox(width: 30),
+      Text(recipe.name),
+      SizedBox(width: 60),
+      IconButton(
+        onPressed: (){
+          uiController.deselectRecipe();
+          }, 
+        icon: Icon(Icons.close)
+        )
+      ],
     );
   }
 }
